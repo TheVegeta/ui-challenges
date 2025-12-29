0d57f998588daa95bcfa81725f58ef7c05e6deb5
TheVegeta <iamvegeta@protonmail.com>
2025-12-29 18:31:37 +0530

    restructure

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: "index.html",
+        page1: "practice/1.html",
+      },
+      output: {
+        entryFileNames: "[hash].js",
+        chunkFileNames: "[hash].js",
+        assetFileNames: "[hash].[ext]",
+      },
+    },
+  },
 });
